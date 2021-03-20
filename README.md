045832c4b0cd515da458560e81bae734e9543563
Joseph Carr <joseph.carr@mail.mcgill.ca>
2021-03-19 21:08:52 -0400

    Code for directory hashing

--- README.md
+++ README.md
@@ -4,3 +4,4 @@ A Qt project for generating cryptographic hashes of data backups
 ## Resources
 - OpenSSL cryptography library
    - https://www.openssl.org/docs/manmaster/man3/SHA512.html
+   - https://en.wikipedia.org/wiki/Secure_Hash_Algorithms
--- mainwindow.cpp
+++ mainwindow.cpp
@@ -40,8 +40,8 @@ void copyDirectory(QString source, QString destination, bool lock = true)
         qDebug("source directory path: %s", qPrintable(subSource_path));
         qDebug("destination directory path: %s", qPrintable(subDestination_path));
         sourceDirectory.mkpath(subDestination_path);
-        if(lock) copyDirectory(subSource_path, subDestination_path);
-        //QFile(subDestination_path).setPermissions(QFileDevice::ReadOwner);
+        copyDirectory(subSource_path, subDestination_path);
+        //if(lock) QFile(subDestination_path).setPermissions(QFileDevice::ReadOwner);
     }
 
     foreach (QString file, sourceDirectory.entryList(QDir::Files)) {
@@ -61,32 +61,49 @@ void copyDirectory(QString source, QString destination, bool lock = true)
  * A function for computing the cryptographic hash of a function.
  * Uses OpenSSL's implementation of the SHA512 hashing algorithm.
  */
-QByteArray cryptoHash(QString file_name)
+QByteArray hashFile(QString file_name)
 {
     const int buffer_size = 1024*1024;
-    QByteArray returnHash(SHA512_DIGEST_LENGTH, (unsigned char) 0);
-    SHA512_CTX hash;
-    if(!SHA512_Init(&hash)) return returnHash;
+    QByteArray returnHash(SHA256_DIGEST_LENGTH, (unsigned char) 0);
+    SHA256_CTX hash;
+    if(!SHA256_Init(&hash)) return returnHash;
 
     QFile file(file_name);
-    if(file.open(QIODevice::ReadOnly)){
-            char buffer[buffer_size];
+    if(file.open(QIODevice::ReadOnly))
+    {
+        char buffer[buffer_size];
 
-            int remainingBytes = file.size();
-            int bytesRead;
-            int nextReadSize = qMin(remainingBytes, buffer_size);
-            while (nextReadSize > 0 && (bytesRead = file.read(buffer, nextReadSize)) > 0)
-            {
-                        qDebug("read bytes: %s", qPrintable(QByteArray(buffer).toHex()));
-                        remainingBytes -= bytesRead;
-                        if(!SHA512_Update(&hash, buffer, bytesRead)) return returnHash;
-                        nextReadSize = qMin(remainingBytes, buffer_size);
-            }
-            file.close();
+        int remainingBytes = file.size();
+        int bytesRead;
+        int nextReadSize = qMin(remainingBytes, buffer_size);
+        while (nextReadSize > 0 && (bytesRead = file.read(buffer, nextReadSize)) > 0)
+        {
+            qDebug("read bytes: %s", qPrintable(QByteArray(buffer, bytesRead).toHex()));
+            remainingBytes -= bytesRead;
+            if(!SHA256_Update(&hash, buffer, bytesRead)) return returnHash;
+            nextReadSize = qMin(remainingBytes, buffer_size);
+        }
+        file.close();
     }
-    if(!SHA512_Final((unsigned char*)returnHash.data(), &hash)) return returnHash;
-    qDebug("Hash value: %s", qPrintable(returnHash.toHex()));
+    if(!SHA256_Final((unsigned char*)returnHash.data(), &hash)) return returnHash;
+    qDebug("Hash value: %s\n", qPrintable(returnHash.toHex()));
     return returnHash;
+}
+
+void hashDirectory(QString path){
+    QDir directory(path);
+    if(!directory.exists()) return;
+
+    foreach (QString subdirectory, directory.entryList(QDir::Dirs | QDir::NoDotAndDotDot)) {
+        QString sub_path = path + QDir::separator() + subdirectory;
+        hashDirectory(sub_path);
+    }
+
+    foreach (QString file, directory.entryList(QDir::Files)) {
+        QString file_path = path + QDir::separator() + file;
+        qDebug("file path: %s", qPrintable(file_path));
+        hashFile(file_path);
+    }
 }
 
 void MainWindow::on_IPPathButton_clicked()
@@ -101,7 +118,8 @@ void MainWindow::on_IPBackupPathButton_clicked()
 {
     QString file_name = QFileDialog::getExistingDirectory(this, "Select Directory", QDir::homePath());
     qDebug("IP Backup Path: %s", qPrintable(file_name));
-    if(!file_name.isEmpty()){
+    if(!file_name.isEmpty())
+    {
         ui->IPBackupPathInput->setText(file_name);
         if(ui->HashBackupPathInput->text().isEmpty())
             ui->HashBackupPathInput->setText(file_name);
@@ -110,6 +128,7 @@ void MainWindow::on_IPBackupPathButton_clicked()
 
 void MainWindow::on_BackupButton_clicked()
 {
+    qDebug("Backing up %s", qPrintable(ui->IPPathInput->text()));
     copyDirectory(ui->IPPathInput->text(), ui->IPBackupPathInput->text());
 }
 
@@ -136,5 +155,5 @@ void MainWindow::on_HashSavePathButton_clicked()
 
 void MainWindow::on_HashButton_clicked()
 {
-    cryptoHash(ui->HashBackupPathInput->text());
+    hashDirectory(ui->HashBackupPathInput->text());
 }
